d3d55dd01681489cbe73bc465c561c875585f2ba
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-02-25 21:11:51 -0600

    Added Style to Categories

--- src/components/BaseTitle.vue
+++ src/components/BaseTitle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center justify-center gap-4">
-        <h1 class="text-6xl font-thin text-slate-800 uppercase">
+        <h1 class="text-6xl font-thin text-blue-600 uppercase">
             <slot/>
         </h1>
     </div>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,26 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import BaseTitle from '@/components/BaseTitle.vue';
+import useAPI from '@/composables/useAPI'
+
+const { categories, getCategories } = useAPI()
+
+onMounted(async () => {
+  await getCategories()
+})
+
+
 </script>
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
+  <div class="grid flex-grow grid=cols-4 gap-12 m-20">
+  <RouterLink v-for="category in categories" 
+  :key="category.id"
+  :to="''"
+  class="bg-white text-center flex h-32 items-center justify-center rounded-lg border-6 border-slate-600 py-4 font-bold uppercase hover:cursor-pointer hover:border-red-700 hover:bg-red-400 hover:text-white transition-colors duration-30"
+  >
+    {{ category.name }}
+  </RouterLink>
+  </div>
 </template>
