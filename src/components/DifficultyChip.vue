91f0569141f9311d767ec80e9e459b45b459cdfd
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-05-04 15:30:46 -0500

    difficulty added

---
+++ src/components/DifficultyChip.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    difficulty: {
+        type: String, 
+        required: true, 
+    }
+})
+</script>
+
+<template>
+    <P class="px-4 py-2 rounded-full border absolute uppercase text-white bottom-5 left-8 text-2xl font-bold"
+    :class="props.difficulty === 'easy' ? 'bg-green-800' : props.difficulty === 'medium' ? 'bg-yellow-700' : 'bg-red-800' ">
+    {{ props.difficulty }}
+    </P>
+</template>
